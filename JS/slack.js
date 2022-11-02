9235105a8b137d71913b55f573bf0d1518f73df4
Bamideleaduke <bamideleaduke@gmail.com>
2022-11-02 21:46:53 +0100

    header and hero section

---
+++ JS/slack.js
@@ -0,0 +1,16 @@
+// BARAKAT
+const menuIcon = document.querySelector(".menu-icon");
+const menuItems = document.querySelector(".menu-items");
+const collapse = document.querySelector(".collapsible");
+const closeBtn = document.querySelector(".close-icon");
+// 
+menuIcon.addEventListener("click", ()=>{
+    collapse.classList.toggle("hide");
+})
+closeBtn.addEventListener("click", ()=>{
+    collapse.classList.toggle("hide");
+})
+// BARAKAT
+
+
+
