17a4805f8915daf1b61c8f9abf010cb0b3b924bd
favy-codez <107569845+favy-codez@users.noreply.github.com>
2022-11-14 22:00:49 +0100

    commit message

--- JS/slack.js
+++ JS/slack.js
@@ -84,3 +84,58 @@ function footer6() {
     }
   }
 //FAVOUR END
+
+// nav scroll funtionality
+window.onscroll = function() {scrollFunction()};
+const header = document.querySelector("header")
+const searchIcon = document.querySelector(".search-icon1 .fa-solid")
+const searchIcon2 = document.querySelector(".search-icon2 .fa-solid")
+const menuIcon1 = document.querySelector(".menu-icon .fa-solid")
+const logo1 = document.querySelector('.logo1')
+const logo = document.querySelector('.logo')
+const menuItem = document.querySelectorAll('.menu-items .menu-link a')
+const signIn = document.querySelector(".signIn")
+const tSales = document.querySelector(".tSales")
+const nWorkspace = document.querySelector(".nWorkspace")
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    header.style.top = "0";
+    header.style.backgroundColor = "#fff";
+    header.style.position = "fixed"
+    header.style.width = "100%"
+    header.style.transition = "top .7s"
+    header.style.zIndex = "999"
+    searchIcon.style.color = "black"
+    menuIcon1.style.color = "black"
+    logo.style.display = "none"
+    logo1.style.display = "block"
+    menuItem.forEach(item => {
+      item.style.color = "black"
+    })
+    signIn.style.color = "black"
+    searchIcon2.style.color = "black"
+    tSales.style.color = "#4a154b"
+    tSales.style.border = "2px solid #4a154b"
+    nWorkspace.style.backgroundColor = "#4a154b"
+    nWorkspace.style.color = "white"
+  } else {
+    header.style.top = "-50px";
+    header.style.backgroundColor = "#4a154b";
+    header.style.position = "sticky";
+    header.style.transition = "top .7s"
+    searchIcon.style.color = "white"
+    menuIcon1.style.color = "white"
+    logo1.style.display = "none"
+    logo.style.display = "block"
+    menuItem.forEach(item => {
+      item.style.color = "white"
+    })
+    signIn.style.color = "white"
+    searchIcon2.style.color = "white"
+    tSales.style.color = "white"
+    tSales.style.border = "2px solid white"
+    nWorkspace.style.backgroundColor = "white"
+    nWorkspace.style.color = "#4a154b"
+  }
+}
